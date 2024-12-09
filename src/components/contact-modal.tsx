70cba1b76ad5a4449094d1966899bb4bcc5dd0ae
chris-june <chris.june@intellisync.ca>
2024-12-09 17:01:15 -0500

    refactor: remove unused DialogPortal import from contact-modal

--- src/components/contact-modal.tsx
+++ src/components/contact-modal.tsx
@@ -6,7 +6,6 @@ import {
   DialogTitle,
   DialogDescription,
   DialogFooter,
-  DialogPortal
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
